21c202122082c06a6fd48967bfdb4e103dc77edf
Wojciech Malarski <w.malarski@gmail.com>
2022-11-13 00:25:53 +0100

    comment out auth0

--- src/server/auth/options.ts
+++ src/server/auth/options.ts
@@ -1,16 +1,16 @@
 import type { NextAuthOptions } from "next-auth/core/types";
-import Auth0Provider from "next-auth/providers/auth0";
+// import Auth0Provider from "next-auth/providers/auth0";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { env } from "../env";
 
 export const authOptions: NextAuthOptions = {
   secret: env.VITE_NEXTAUTH_SECRET,
   providers: [
-    Auth0Provider({
-      clientId: env.VITE_AUTH0_CLIENT_ID,
-      clientSecret: env.VITE_AUTH0_CLIENT_SECRET,
-      issuer: env.VITE_AUTH0_ISSUER,
-    }),
+    // Auth0Provider({
+    //   clientId: env.VITE_AUTH0_CLIENT_ID,
+    //   clientSecret: env.VITE_AUTH0_CLIENT_SECRET,
+    //   issuer: env.VITE_AUTH0_ISSUER,
+    // }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
--- src/server/env.ts
+++ src/server/env.ts
@@ -3,9 +3,9 @@ import { z } from "zod";
 const envScheme = z.object({
   VITE_NEXTAUTH_URL: z.string(),
   VITE_NEXTAUTH_SECRET: z.string(),
-  VITE_AUTH0_CLIENT_ID: z.string(),
-  VITE_AUTH0_CLIENT_SECRET: z.string(),
-  VITE_AUTH0_ISSUER: z.string(),
+  // VITE_AUTH0_CLIENT_ID: z.string(),
+  // VITE_AUTH0_CLIENT_SECRET: z.string(),
+  // VITE_AUTH0_ISSUER: z.string(),
 });
 
 if (typeof window !== "undefined") {
