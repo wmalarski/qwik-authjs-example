59da809549beffe51c3f771c219027bf5acfb23a
Wojciech Malarski <w.malarski@gmail.com>
2023-01-23 20:59:55 +0100

    yet another try before resiging

--- src/server/auth/auth.ts
+++ src/server/auth/auth.ts
@@ -1,7 +1,10 @@
 import { Auth, type AuthConfig } from "@auth/core";
 import { AuthAction, Session } from "@auth/core/types";
-import type { loader$, RequestEvent } from "@builder.io/qwik-city";
+import type { RequestEvent } from "@builder.io/qwik-city";
+import { serialize } from "cookie";
 import crypto from "crypto";
+import { parseString, splitCookiesString } from "set-cookie-parser";
+import type { ActionFunction, RequestEventLoader } from "../types";
 
 // This solves issue with @auth/core I have.
 // [vite] Internal server error: crypto is not defined
@@ -30,24 +33,26 @@ const actions: AuthAction[] = [
 // currently multiple cookies are not supported, so we keep the next-auth.pkce.code_verifier cookie for now:
 // because it gets updated anyways
 // src: https://github.com/solidjs/solid-start/issues/293
-// const getSetCookieCallback = (cookie?: string | null) => {
-//   // if (!cook) return
-//   // const splitCookie = splitCookiesString(cook)
-//   // for (const cookName of [
-//   //   "__Secure-next-auth.session-token",
-//   //   "next-auth.session-token",
-//   //   "next-auth.pkce.code_verifier",
-//   //   "__Secure-next-auth.pkce.code_verifier",
-//   // ]) {
-//   //   const temp = splitCookie.find((e) => e.startsWith(`${cookName}=`))
-//   //   if (temp) {
-//   //     return parseString(temp)
-//   //   }
-//   // }
-//   // return parseString(splitCookie?.[0] ?? "") // just return the first cookie if no session token is found
-//   console.log(cookie);
-//   return [];
-// };
+const getSetCookieCallback = (cookie?: string | null) => {
+  if (!cookie) return;
+  const splitCookie = splitCookiesString(cookie);
+
+  console.log({ splitCookie });
+
+  for (const cookName of [
+    "__Secure-next-auth.session-token",
+    "next-auth.session-token",
+    "next-auth.pkce.code_verifier",
+    "__Secure-next-auth.pkce.code_verifier",
+  ]) {
+    const temp = splitCookie.find((e) => e.startsWith(`${cookName}=`));
+    if (temp) {
+      return parseString(temp);
+    }
+  }
+  console.log({ cookie, splitCookie });
+  return parseString(splitCookie?.[0] ?? ""); // just return the first cookie if no session token is found
+};
 
 const QwikAuthHandler = (prefix: string, authOptions: QwikAuthConfig) => {
   return async (event: RequestEvent) => {
@@ -65,24 +70,77 @@ const QwikAuthHandler = (prefix: string, authOptions: QwikAuthConfig) => {
 
     const res = await Auth(request as any, authOptions);
 
+    console.log({
+      res,
+      action,
+      body: res.body,
+      red: res.redirected,
+      d: res.url,
+    });
+
     if (["callback", "signin", "signout"].includes(action)) {
       const resCookies = res.clone().headers.get("Set-Cookie");
-
+      console.log({ resCookies });
       if (resCookies) {
         event.cookie.set("Set-Cookie", resCookies);
       }
 
-      // const parsedCookie = getSetCookieCallback(
-      //   res.clone().headers.get("Set-Cookie")
-      // );
+      const parsedCookie = getSetCookieCallback(resCookies);
       //   res.headers.delete("Set-Cookie");
 
-      // if (parsedCookie) {
-      //   res.headers.set(
-      //     "Set-Cookie",
-      //     serialize(parsedCookie.name, parsedCookie.value, parsedCookie as any)
-      //   );
-      // }
+      console.log({ parsedCookie });
+
+      if (parsedCookie) {
+        res.headers.set(
+          "Set-Cookie",
+          serialize(parsedCookie.name, parsedCookie.value, parsedCookie as any)
+        );
+      }
+    }
+
+    event.status(res.status);
+
+    event.send(res.status, "");
+
+    // console.log({ res, body: res.body, headers: res.headers, r: res.url });
+
+    return res;
+  };
+};
+
+const QwikAuthActionHandler = (
+  action: AuthAction,
+  authOptions: QwikAuthConfig
+): ActionFunction => {
+  return async (_form, event) => {
+    const res = await Auth(event.request as any, authOptions);
+
+    console.log({
+      res,
+      action,
+      body: res.body,
+      red: res.redirected,
+      d: res.url,
+    });
+
+    if (["callback", "signin", "signout"].includes(action)) {
+      const resCookies = res.clone().headers.get("Set-Cookie");
+      console.log({ resCookies });
+      if (resCookies) {
+        event.cookie.set("Set-Cookie", resCookies);
+      }
+
+      const parsedCookie = getSetCookieCallback(resCookies);
+      //   res.headers.delete("Set-Cookie");
+
+      console.log({ parsedCookie });
+
+      if (parsedCookie) {
+        res.headers.set(
+          "Set-Cookie",
+          serialize(parsedCookie.name, parsedCookie.value, parsedCookie as any)
+        );
+      }
     }
 
     event.status(res.status);
@@ -118,8 +176,23 @@ export const QwikAuth = (config: QwikAuthConfig) => {
   };
 };
 
-type LoaderFunction = Parameters<typeof loader$>[0];
-type RequestEventLoader = Parameters<LoaderFunction>[0];
+export const QwikAuthAction = (
+  action: AuthAction,
+  config: QwikAuthConfig
+): ActionFunction => {
+  const authOptions = { ...config };
+
+  authOptions.secret ??= process.env.AUTH_SECRET;
+
+  authOptions.trustHost ??= !!(
+    process.env.AUTH_TRUST_HOST ??
+    process.env.VERCEL ??
+    process.env.NODE_ENV !== "production"
+  );
+
+  return QwikAuthActionHandler(action, authOptions);
+};
+
 export type GetSessionResult = Promise<Session | null>;
 
 export const getSession = async (
--- src/server/types.ts
+++ src/server/types.ts
@@ -1,4 +1,5 @@
-import type { loader$ } from "@builder.io/qwik-city";
+import type { action$, loader$ } from "@builder.io/qwik-city";
 
 type LoaderFunction = Parameters<typeof loader$>[0];
 export type RequestEventLoader = Parameters<LoaderFunction>[0];
+export type ActionFunction = Parameters<typeof action$>[0];
